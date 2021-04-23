8a01c06b89fe0db007e2a93d27e7ad4974e745e9
thiago miziara <thiagonmiziara@gmail.com>
2021-04-22 21:25:02 -0300

    add pagina home e seus estilos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
+
+import Header from '../components/Header';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,9 +28,53 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum titulo</strong>
+              <p>afdasdfasdfasdfasdfasdfasdf56asdf54as</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Thiago Nunes
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum titulo</strong>
+              <p>afdasdfasdfasdfasdfasdfasdf56asdf54as</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Thiago Nunes
+                </li>
+              </ul>
+            </a>
+          </Link>
+
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
