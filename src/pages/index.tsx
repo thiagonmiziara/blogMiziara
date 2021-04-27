009ccb27568fed57589c9f10a8e3547de479ef01
thiago miziara <thiagonmiziara@gmail.com>
2021-04-27 19:39:07 -0300

    configurando variaveis de ambiente e pengado os posts do prismic CMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
 
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import Header from '../components/Header';
@@ -28,46 +34,48 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
+
   return (
     <>
       <main className={commonStyles.container}>
         <Header />
 
         <div className={styles.posts}>
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>Algum titulo</strong>
-              <p>afdasdfasdfasdfasdfasdfasdf56asdf54as</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  15 Mar 2021
-                </li>
-                <li>
-                  <FiUser />
-                  Thiago Nunes
-                </li>
-              </ul>
-            </a>
-          </Link>
-
-          <Link href="/">
-            <a className={styles.post}>
-              <strong>Algum titulo</strong>
-              <p>afdasdfasdfasdfasdfasdfasdf56asdf54as</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  15 Mar 2021
-                </li>
-                <li>
-                  <FiUser />
-                  Thiago Nunes
-                </li>
-              </ul>
-            </a>
-          </Link>
+          {posts.map(post => {
+            return (
+              <Link href={`/post/${post.uid}`} key={post.uid}>
+                <a className={styles.post}>
+                  <strong>{post.data.title}</strong>
+                  <p>{post.data.subtitle}</p>
+                  <ul>
+                    <li>
+                      <FiCalendar />
+                      {post.first_publication_date}
+                    </li>
+                    <li>
+                      <FiUser />
+                      {post.data.author}
+                    </li>
+                  </ul>
+                </a>
+              </Link>
+            );
+          })}
 
           <button type="button">Carregar mais posts</button>
         </div>
@@ -76,9 +84,35 @@ export default function Home() {
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      pageSize: 100,
+    }
+  );
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
